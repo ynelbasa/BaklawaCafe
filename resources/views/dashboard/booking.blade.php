b0c50aa61dc8e7670defdd329af1185c2ca38613
ynelbasa <marinelbasa@gmail.com>
2020-08-18 10:14:36 +1200

    Add layout files for both dashaboard and client app

---
+++ resources/views/dashboard/booking.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+
+@section('content')
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        <div class="title m-b-md">
+            Baklawa Cafe Dashboard
+        </div>
+    </div>
+</div>
+@endsection
