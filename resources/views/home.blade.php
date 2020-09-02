d6a59bc63d76715a23245e871e2c9af87cd1c2fe
ynelbasa <marinelbasa@gmail.com>
2020-09-03 00:36:38 +1200

    Add bootstrap with auth and sass via npm

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,23 @@
-@extends('layout')
+@extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
-    <div class="flex-center position-ref full-height">
-        <div class="content">
-            <div class="title m-b-md">
-                Baklawa Cafe - Home Page
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
