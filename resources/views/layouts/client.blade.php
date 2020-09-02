3973710ac7011e99d644fdbb2a4e9897491c3ac6
ynelbasa <marinelbasa@gmail.com>
2020-09-03 01:53:29 +1200

    Add layout for client site and setup homepage

--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -3,57 +3,101 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>Baklawa Cafe - Home</title>
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <!-- Scripts -->
+    <script src="{{ asset('../js/app.js') }}" defer></script>
+    <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
 
     <!-- Styles -->
-    <style>
-        html, body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Nunito', sans-serif;
-            font-weight: 200;
-            height: 100vh;
-            margin: 0;
-        }
+    <link href="{{ asset('../css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('../css/client.css') }}" rel="stylesheet">
 
-        .full-height {
-            height: 100vh;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-    </style>
 </head>
 <body>
+<!-- Top Bar  -->
+<div class="top-bar">
+    <div class="container">
+        <div class="col-12 text-center">
+            <p> 46 Stoddard Road, Mount Roskill, Auckland 1041,
+                <a href="tel:09-620 5400">Call us: 09-620 5400</a>
+                Ext 4
+            </p>
+        </div>
+    </div>
+</div>
+<!-- End Top Bar -->
+<!-- Navigation -->
+<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top navbar-custom">
+    <div class="container">
+        <a href="index.html" class="navbar-brand">
+            <img src="image/logo1.png" alt=""/>
+        </a>
+        <button class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarResponsive">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a href="index.html" class="nav-link active">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="foodmenu.html" class="nav-link">Food Menu</a>
+                </li>
+                <li class="nav-item">
+                    <a href="gallery.html" class="nav-link">Gallery</a>
+                </li>
+                <li class="nav-item">
+                    <a href="ourstory.html" class="nav-link">Our Story</a>
+                </li>
+                <li class="nav-item">
+                    <a class="btn btn-sm btn-outline-secondary nav-button"
+                       type="button"
+                       href="booking.html"> Book A Table! </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- End Navigation -->
 @yield('content')
+<!--- Footer -->
+<footer>
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-md-3 social padding">
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+            </div>
+            <div class="col-md-9">
+                <ul class="ml-auto footer-nav">
+                    <li class="">
+                        <a href="#" class="">Home</a>
+                    </li>
+                    <li class="">
+                        <a href="#" class="">Food Menu</a>
+                    </li>
+                    <li class="">
+                        <a href="#" class="">Gallery</a>
+                    </li>
+                    <li class="">
+                        <a href="#" class="">Our Story</a>
+                    </li>
+                    <li class="">
+                        <a href="#" class="">Book A Table!</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+<div class="col-12 text-center">
+    <h5 style="font-size: 13px; color: black; opacity: 0.5">
+        &copy; Marinel & Subash
+    </h5>
+</div>
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', `public/js`)
+    .js('resources/js/client.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/client.scss', 'public/css')
     .sass('resources/sass/dashboard.scss', 'public/css');
