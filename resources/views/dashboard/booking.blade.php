d93a052f6bfecaa318844b072301da3320a84de4
ynelbasa <marinelbasa@gmail.com>
2020-09-27 16:03:46 +1300

    Show informative text and hide table when there are no results

--- resources/views/dashboard/booking.blade.php
+++ resources/views/dashboard/booking.blade.php
@@ -37,58 +37,63 @@
                 </div>
             </div>
             <br>
-            <div class="table-responsive">
-                <table class="table table-hover">
-                    <thead class="text-dark">
-                    <tr>
-                        <th scope="col"></th>
-                        <th scope="col">Customer</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Phone</th>
-                        <th scope="col">Date</th>
-                        <th scope="col">Table</th>
-                        <th scope="col">Notes</th>
-                        <th scope="col">Status</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($bookings as $booking)
+            @if( count($bookings) === 0)
+                <h5 class="text-center m-5">No records found that matches your criteria</h5>
+            @endif
+            @if( count($bookings) > 0)
+                <div class="table-responsive">
+                    <table class="table table-hover">
+                        <thead class="text-dark">
                         <tr>
-                            <td>{{ $booking->id }}</td>
-                            <td>{{ $booking->first_name }} {{ $booking->last_name }}</td>
-                            <td>{{ $booking->email_address }}</td>
-                            <td>{{ $booking->phone_number }}</td>
-                            <td>{{ $booking->date_time }}</td>
-                            <td class="text-lg-center">
-                                <h5>
-                                    {{ $booking->table_size }}
-                                    <i class="fas fa-user"></i></h5>
-                            </td>
-                            <td>{{ $booking->notes }}</td>
-                            <td>
-                                <div class="dropdown w-auto">
-                                    <button
-                                        class="btn btn-secondary dropdown-toggle border-0 text-uppercase {{$booking->status}}"
-                                        type="button"
-                                        id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
-                                        aria-expanded="false">
-                                        {{$booking->status}}
-                                    </button>
-                                    <div class="dropdown-menu text-uppercase" aria-labelledby="dropdownMenuButton">
-                                        <a class="dropdown-item text-confirmed" href="#">
-                                            Confirmed</a>
-                                        <a class="dropdown-item text-pending" href="#">
-                                            Pending</a>
-                                        <a class="dropdown-item text-cancelled" href="#">
-                                            Cancelled</a></div>
-                                </div>
-                            </td>
+                            <th scope="col"></th>
+                            <th scope="col">Customer</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Phone</th>
+                            <th scope="col">Date</th>
+                            <th scope="col">Table</th>
+                            <th scope="col">Notes</th>
+                            <th scope="col">Status</th>
                         </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-                {{ $bookings->appends(['status' => $status])->links() }}
-            </div>
+                        </thead>
+                        <tbody>
+                        @foreach($bookings as $booking)
+                            <tr>
+                                <td>{{ $booking->id }}</td>
+                                <td>{{ $booking->first_name }} {{ $booking->last_name }}</td>
+                                <td>{{ $booking->email_address }}</td>
+                                <td>{{ $booking->phone_number }}</td>
+                                <td>{{ $booking->date_time }}</td>
+                                <td class="text-lg-center">
+                                    <h5>
+                                        {{ $booking->table_size }}
+                                        <i class="fas fa-user"></i></h5>
+                                </td>
+                                <td>{{ $booking->notes }}</td>
+                                <td>
+                                    <div class="dropdown w-auto">
+                                        <button
+                                            class="btn btn-secondary dropdown-toggle border-0 text-uppercase {{$booking->status}}"
+                                            type="button"
+                                            id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+                                            aria-expanded="false">
+                                            {{$booking->status}}
+                                        </button>
+                                        <div class="dropdown-menu text-uppercase" aria-labelledby="dropdownMenuButton">
+                                            <a class="dropdown-item text-confirmed" href="#">
+                                                Confirmed</a>
+                                            <a class="dropdown-item text-pending" href="#">
+                                                Pending</a>
+                                            <a class="dropdown-item text-cancelled" href="#">
+                                                Cancelled</a></div>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                    {{ $bookings->appends(['status' => $status])->links() }}
+                </div>
+            @endif
         </div>
     </div>
 @endsection
