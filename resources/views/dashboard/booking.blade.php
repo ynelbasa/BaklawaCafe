59e9e11593c3e5d94a7aae6270420e42ba12f8c8
ynelbasa <marinelbasa@gmail.com>
2020-09-29 23:51:37 +1300

    Fix responsiveness issue

--- resources/views/dashboard/booking.blade.php
+++ resources/views/dashboard/booking.blade.php
@@ -7,92 +7,87 @@
         <div class="bg-white p-4 box-shadow rounded-lg">
             <h2 class="text-dark">Bookings</h2>
             <div class="row">
-                <div class="col-sm-2">
-                    <h5 class="m-4">Filter by Status</h5>
+                <div class="col-sm-2 m-4">
+                    <h5>Filter by Status</h5>
                 </div>
-                <div class="col-sm-7">
-                    <div class="status-option m-3 row">
-                        <div class="col-sm-2 m-2">
-                            <a class="{{ $status == 'confirmed' ? 'active' : '' }}"
-                               href="{{ url('/dashboard/booking?status=confirmed') }}">Confirmed</a>
-                        </div>
-                        <div class="col-sm-2 m-2">
-                            <a class="{{ $status == 'pending' ? 'active' : ''}}"
-                               href="{{ url('/dashboard/booking?status=pending') }}">Pending</a>
-                        </div>
-                        <div class="col-sm-2 m-2">
-                            <a class="{{ $status == 'cancelled' ? 'active' : ''}}"
-                               href="{{ url('/dashboard/booking?status=cancelled') }}">Cancelled</a>
-                        </div>
-                        <div class="col-sm-2 m-2">
-                            <a class="{{ $status ? '' : 'active'}}" href="{{ url('/dashboard/booking') }}">All</a>
-                        </div>
+                <div class="status-option">
+                    <div class="col-sm-1 m-4 text-center">
+                        <a class="{{ $status == 'confirmed' ? 'active' : '' }}"
+                           href="{{ url('/dashboard/booking?status=confirmed') }}">Confirmed</a>
+                    </div>
+                    <div class="col-sm-1 m-4 text-center">
+                        <a class="{{ $status == 'pending' ? 'active' : ''}}"
+                           href="{{ url('/dashboard/booking?status=pending') }}">Pending</a>
+                    </div>
+                    <div class="col-sm-1 m-4 text-center">
+                        <a class="{{ $status == 'cancelled' ? 'active' : ''}}"
+                           href="{{ url('/dashboard/booking?status=cancelled') }}">Cancelled</a>
+                    </div>
+                    <div class="col-sm-1 m-4 text-center">
+                        <a class="{{ $status ? '' : 'active'}}" href="{{ url('/dashboard/booking') }}">All</a>
                     </div>
                 </div>
-                <div class="col-sm-3 my-3 text-center">
-                </div>
-            </div>
-            <br>
-            @if( count($bookings) === 0)
-                <h5 class="text-center m-5">No records found that matches your criteria</h5>
-            @endif
-            @if( count($bookings) > 0)
-                <div class="table-responsive">
-                    <table class="table table-hover">
-                        <thead class="text-dark">
-                        <tr>
-                            <th scope="col"></th>
-                            <th scope="col">Customer</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Phone</th>
-                            <th scope="col">Reserved Date</th>
-                            <th scope="col">Table</th>
-                            <th scope="col">Notes</th>
-                            <th scope="col">Status</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($bookings as $booking)
+                @if( count($bookings) === 0)
+                    <h5 class="text-center m-5">No records found that matches your criteria</h5>
+                @endif
+                @if( count($bookings) > 0)
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <thead class="text-dark">
                             <tr>
-                                <td>{{ $booking->id }}</td>
-                                <td>{{ $booking->first_name }} {{ $booking->last_name }}</td>
-                                <td>{{ $booking->email_address }}</td>
-                                <td>{{ $booking->phone_number }}</td>
-                                <td>{{ $booking->date_time }}</td>
-                                <td class="text-lg-center">
-                                    <h5>
-                                        {{ $booking->table_size }}
-                                        <i class="fas fa-user"></i></h5>
-                                </td>
-                                <td>{{ $booking->notes }}</td>
-                                <td>
-                                    <div class="dropdown w-auto">
-                                        <button
-                                            class="btn btn-secondary dropdown-toggle border-0 text-uppercase {{$booking->status}}"
-                                            type="button"
-                                            id="statusDropdown_{{$booking->id}}" data-toggle="dropdown"
-                                            aria-haspopup="true"
-                                            aria-expanded="false">
-                                            {{$booking->status}}
-                                        </button>
-                                        <div class="dropdown-menu text-uppercase" aria-labelledby="dropdownMenuButton">
-                                            <a class="dropdown-item"
-                                               onclick="updateStatus( {{ $booking->id }}, 'confirmed')">Confirmed</a>
-                                            <a class="dropdown-item"
-                                               onclick="updateStatus( {{ $booking->id }}, 'pending')">
-                                                Pending</a>
-                                            <a class="dropdown-item"
-                                               onclick="updateStatus( {{ $booking->id }}, 'cancelled')">
-                                                Cancelled</a></div>
-                                    </div>
-                                </td>
+                                <th scope="col"></th>
+                                <th scope="col">Customer</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Phone</th>
+                                <th scope="col">Reserved Date</th>
+                                <th scope="col">Table</th>
+                                <th scope="col">Notes</th>
+                                <th scope="col">Status</th>
                             </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                    {{ $bookings->appends(['status' => $status])->links() }}
-                </div>
-            @endif
+                            </thead>
+                            <tbody>
+                            @foreach($bookings as $booking)
+                                <tr>
+                                    <td>{{ $booking->id }}</td>
+                                    <td>{{ $booking->first_name }} {{ $booking->last_name }}</td>
+                                    <td>{{ $booking->email_address }}</td>
+                                    <td>{{ $booking->phone_number }}</td>
+                                    <td>{{ $booking->date_time }}</td>
+                                    <td class="text-lg-center">
+                                        <h5>
+                                            {{ $booking->table_size }}
+                                            <i class="fas fa-user"></i></h5>
+                                    </td>
+                                    <td>{{ $booking->notes }}</td>
+                                    <td>
+                                        <div class="dropdown w-auto">
+                                            <button
+                                                class="btn btn-secondary dropdown-toggle border-0 text-uppercase {{$booking->status}}"
+                                                type="button"
+                                                id="statusDropdown_{{$booking->id}}" data-toggle="dropdown"
+                                                aria-haspopup="true"
+                                                aria-expanded="false">
+                                                {{$booking->status}}
+                                            </button>
+                                            <div class="dropdown-menu text-uppercase"
+                                                 aria-labelledby="dropdownMenuButton">
+                                                <a class="dropdown-item"
+                                                   onclick="updateStatus( {{ $booking->id }}, 'confirmed')">Confirmed</a>
+                                                <a class="dropdown-item"
+                                                   onclick="updateStatus( {{ $booking->id }}, 'pending')">
+                                                    Pending</a>
+                                                <a class="dropdown-item"
+                                                   onclick="updateStatus( {{ $booking->id }}, 'cancelled')">
+                                                    Cancelled</a></div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                        {{ $bookings->appends(['status' => $status])->links() }}
+                    </div>
+                @endif
+            </div>
         </div>
-    </div>
 @endsection
--- resources/views/dashboard/menu.blade.php
+++ resources/views/dashboard/menu.blade.php
@@ -8,74 +8,69 @@
         <div class="bg-white p-4 box-shadow rounded-lg">
             <h2 class="text-dark">Menu</h2>
             <div class="row">
-                <div class="col-sm-3">
-                    <h5 class="m-4">Filter by Menu Type</h5>
+                <div class="col-sm-2 m-4">
+                    <h5>Filter by Menu Type</h5>
                 </div>
-                <div class="col-sm-6">
-                    <div class="status-option m-3 row">
-                        @foreach($menuTypes as $menuType)
-                            <div class="col-sm-2 m-2">
-                                <a class="{{ $type === $menuType->id ? 'active' : '' }}"
-                                   href="{{ url("/dashboard/menu?type=$menuType->id") }}">{{ $menuType->name }}</a>
-                            </div>
-                        @endforeach
-                        <div class="col-sm-2 m-2">
-                            <a class="{{ $type === 0 ? 'active' : '' }}"
-                               href="{{ url("/dashboard/menu") }}">All</a>
+                <div class="status-option">
+                    @foreach($menuTypes as $menuType)
+                        <div class="col-sm-1 m-4 text-center">
+                            <a class="{{ $type === $menuType->id ? 'active' : '' }}"
+                               href="{{ url("/dashboard/menu?type=$menuType->id") }}">{{ $menuType->name }}</a>
                         </div>
+                    @endforeach
+                    <div class="col-sm-1 m-4 text-center">
+                        <a class="{{ $type === 0 ? 'active' : '' }}"
+                           href="{{ url("/dashboard/menu") }}">All</a>
                     </div>
                 </div>
-                <div class="col-sm-3 my-3 text-center">
-                </div>
             </div>
-            <br>
-            @if( count($menuItems) === 0)
-                <h5 class="text-center m-5">No records found that matches your criteria</h5>
-            @endif
-            @if( count($menuItems) > 0)
-                <div class="table-responsive">
-                    <table class="table table-hover ">
-                        <thead class="text-dark">
+        @if( count($menuItems) === 0)
+            <h5 class="text-center m-5">No records found that matches your criteria</h5>
+        @endif
+        @if( count($menuItems) > 0)
+            <div class="table-responsive">
+                <table class="table table-hover ">
+                    <thead class="text-dark">
+                    <tr>
+                        <th scope="col"></th>
+                        <th scope="col">Menu Item</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Menu Type</th>
+                        <th scope="col">Enabled</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($menuItems as $menuItem)
                         <tr>
-                            <th scope="col"></th>
-                            <th scope="col">Menu Item</th>
-                            <th scope="col">Description</th>
-                            <th scope="col">Price</th>
-                            <th scope="col">Menu Type</th>
-                            <th scope="col">Enabled</th>
+                            <td>{{ $menuItem->id }}</td>
+                            <td><img
+                                    src="{{ $menuItem->thumbnail_path }}"
+                                    class="mr-2 rounded-sm menu-icon"
+                                    width="12%"
+                                />
+                                {{ $menuItem->name }}
+                            </td>
+                            <td>{{ $menuItem->description }}</td>
+                            <td>$
+                                {{ $menuItem->price }}</td>
+                            <td>
+                                {{ $menuItem->menuType }}</td>
+                            <td>
+                                <label class="switch">
+                                    <input type="checkbox"
+                                           id="enabledCheckbox_{{$menuItem->id}}"
+                                           onchange="updateEnabled({{$menuItem->id}})"
+                                        {{ $menuItem->enabled ? 'checked' : '' }}>
+                                    <span class="slider round"></span>
+                                </label>
+                            </td>
                         </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($menuItems as $menuItem)
-                            <tr>
-                                <td>{{ $menuItem->id }}</td>
-                                <td><img
-                                        src="{{ $menuItem->thumbnail_path }}"
-                                        class="mr-2 rounded-sm menu-icon"
-                                        width="12%"
-                                    />
-                                    {{ $menuItem->name }}
-                                </td>
-                                <td>{{ $menuItem->description }}</td>
-                                <td>$
-                                    {{ $menuItem->price }}</td>
-                                <td>
-                                    {{ $menuItem->menuType }}</td>
-                                <td>
-                                    <label class="switch">
-                                        <input type="checkbox"
-                                               id="enabledCheckbox_{{$menuItem->id}}"
-                                               onchange="updateEnabled({{$menuItem->id}})"
-                                            {{ $menuItem->enabled ? 'checked' : '' }}>
-                                        <span class="slider round"></span>
-                                    </label>
-                                </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                    {{ $menuItems->appends(['type' => $type > 0 ? $type : null])->links() }}</div>
-            @endif
-        </div>
+                    @endforeach
+                    </tbody>
+                </table>
+                {{ $menuItems->appends(['type' => $type > 0 ? $type : null])->links() }}</div>
+        @endif
+    </div>
     </div>
 @endsection
