f479d4056954f6018fca7694d690aaa524524193
ynelbasa <marinelbasa@gmail.com>
2020-09-30 00:52:44 +1300

    Add bookings and menu items stats

--- app/Http/Controllers/WebControllers/DashboardController.php
+++ app/Http/Controllers/WebControllers/DashboardController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers\WebControllers;
 
 use App\Booking;
 use App\Http\Controllers\Controller;
+use App\MenuItem;
+use App\MenuType;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 
 class DashboardController extends Controller
@@ -26,6 +30,14 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('dashboard.index');
+        $bookingsToday = DB::table('bookings')->whereBetween('created_at', [Carbon::yesterday(), Carbon::today()])->count();
+
+        $menuTypes = MenuType::all();
+        $menuItems = MenuItem::all();
+        foreach ($menuTypes as $menuType) {
+            $menuType->count = $menuItems->where('menu_type_id', $menuType->id)->count();
+        }
+
+        return view('dashboard.index', ['bookingsToday' => $bookingsToday, 'menuTypes' => $menuTypes]);
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -5,45 +5,39 @@
         <img src="/images/logo.png" class="my-3" alt="Logo" height="120"/>
     </div>
     <div class="p4 content">
-        <div class="stats">
-            <div class="bg-white p-4 box-shadow rounded-lg">
+        <div class="stats no-selection">
+            <div class="bg-baklawa-blue p-4 text-white box-shadow rounded-lg">
                 <h5 class="text-secondary">Today</h5>
                 <h3>All Bookings</h3>
                 <br>
                 <h1 class="text-center stats-booking">
-                    72
+                    {{ $bookingsToday }}
                     <i class="fas fa-calendar-check ml-2"></i>
                 </h1>
             </div>
             <div class="bg-white p-4 box-shadow rounded-lg">
                 <h5 class="text-secondary">2020</h5>
                 <h3>Online Bookings</h3>
-                <img src="{{ asset('../../images/chart1.png') }}" width="210px">
-            </div>
-            <div class="bg-white p-4 box-shadow rounded-lg">
-                <h5 class="text-secondary">Total</h5>
-                <h3>Menu Items</h3>
-                <br>
-                <h5 class="text-center stats-menu-breakfast">
-                    16
-                    <i class="fas fa-utensils ml-4"></i>
-                    Breakfast
-                </h5>
-                <h5 class="text-center stats-menu-dinner">
-                    52
-                    <i class="fas fa-utensils ml-4"></i>
-                    Dinner
-                </h5>
-                <h5 class="text-center stats-menu-buffet">
-                    23
-                    <i class="fas fa-utensils ml-4"></i>
-                    Buffet
-                </h5>
+                <img src="{{ asset('../../images/chart1.png') }}" width="250px">
             </div>
             <div class="bg-white p-4 box-shadow rounded-lg">
                 <h5 class="text-secondary">Total</h5>
                 <h3>Website Users</h3><br>
-                <img src="{{ asset('../../images/chart2.png') }}" width="210px"></div>
+                <img src="{{ asset('../../images/chart2.png') }}" width="250px"></div>
+            <div class="bg-baklawa-blue text-white p-4 box-shadow rounded-lg">
+                <h5 class="text-secondary">Total</h5>
+                <h3>Menu Items</h3>
+                <br>
+                @foreach($menuTypes as $menuType)
+                    <a href="{{ url("/dashboard/menu?type=$menuType->id") }}" class="text-decoration-none">
+                        <h5 class="stats-menu">
+                            {{$menuType->count}}
+                            <i class="fas fa-utensils ml-4"></i>
+                            {{$menuType->name}}
+                        </h5>
+                    </a>
+                @endforeach
+            </div>
         </div>
     </div>
 @endsection
